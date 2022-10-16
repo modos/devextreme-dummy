3fe0ef19c7305cf9688eda110acef15419a88113
mohammed hossein mazandaranian <modos.m98@gmail.com>
2022-10-16 19:14:35 +0330

    [library] devextreme added.

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+   <DxDataGrid id="dataGrid">
+            <!-- Configuration goes here -->
+        </DxDataGrid>
+</template>
+
+<script>
+import 'devextreme/dist/css/dx.light.css';
+import { DxDataGrid } from 'devextreme-vue/data-grid';
+export default {
+  name: 'IndexPage',
+  components: {
+        DxDataGrid
+    }
+}
+</script>
