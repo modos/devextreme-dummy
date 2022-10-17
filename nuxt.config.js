9e8c7108abf9570d95d4ac7fed36968c512a9ce6
mohammed hossein mazandaranian <modos.m98@gmail.com>
2022-10-17 15:37:00 +0330

    [ssg] dist router base fixed 3.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: 'devextreme-dummy'
+    base: '/devextreme-dummy/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
